5b63a4a07b468a2493e62d1f993d135da2ecbdae
FabioGarroteDev <fgarrote12@gmail.com>
2022-11-01 23:55:52 -0300

    Tela de Informações básicas pronta e funcionalidade de esconder tela 1 e mostrar tela 2

---
+++ scripts/fabio.js
@@ -0,0 +1,6 @@
+function openCreateQuizz() {
+  const mainLucas = document.querySelector('.lucas')
+  mainLucas.classList.add('hide')
+  const mainFabio = document.querySelector('.fabio')
+  mainFabio.classList.remove('hide')
+}
