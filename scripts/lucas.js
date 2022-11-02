494fdb376341cfc8645149b63608a556f0e885de
lupacco <pagotolc@gmail.com>
2022-11-01 22:29:49 -0300

    Tela 1 UI estrutura OK

---
+++ scripts/lucas.js
@@ -0,0 +1,9 @@
+/**
+ * @brief BUzz Quizz - Lucas Pagotto C. Oliveira - 23/10/22
+ *        Driven Turma 9 - Fourth week of the full-stack course
+ */ 
+
+function hideCreateDiv(){
+    item = document.querySelector('.createQuizz')
+    item.classList.toggle('hide')
+}
