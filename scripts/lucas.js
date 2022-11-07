8065f7e3f7af8ade8211a072b762f644946934e4
Lucas <pagottolc@gmail.com>
2022-11-07 17:45:12 -0300

    Maximo que deu de entregar, faltou alguns bonus

--- scripts/lucas.js
+++ scripts/lucas.js
@@ -3,7 +3,6 @@
  *        Driven Turma 9 - Fourth week of the full-stack course
  */ 
 
-let objects;
 function getQuizzes(){
     axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes')
     .then(quizzes => {
@@ -14,32 +13,49 @@ function getQuizzes(){
 let Quizz;
 //Renderiza quizzes do usuário
 function renderUserQuizzes(){
-    //Resgata objetos salvos em localstorage em formato de string e transforma em JSON
-    let localQuizzes = JSON.parse(localStorage.getItem('userQuizzes'))
-    //console.log(localQuizzes)
-    //Resgata div que contém os quizzes criados pelo usuário
-    let userQuizzesDiv = document.querySelector('.userQuizzes .quizzes')
-    //Percorre cada objeto-quizz do localStorage
-    if(!localQuizzes) return
-    localQuizzes.forEach(quiz => {
-        userQuizzesDiv.innerHTML += `
-        <div class="quizz" id="${quiz.id}" onclick="selectUserQuizz(this)">
-            <div class="gradient"></div>
-            <img src="${quiz.image}" alt="">
-            <div class="quizzOption">
-                <ion-icon name="create-outline"></ion-icon>
-                <ion-icon name="trash-outline"></ion-icon>
+    if(localStorage.getItem('userQuizzes') != null){
+        //Resgata objetos salvos em localstorage em formato de string e transforma em JSON
+        let localQuizzes = JSON.parse(localStorage.getItem('userQuizzes'))
+        //Resgata div que contém os quizzes criados pelo usuário
+        let userQuizzesDiv = document.querySelector('.userQuizzes .quizzes')
+        //Percorre cada objeto-quizz do localStorage
+        if(!localQuizzes) return
+        localQuizzes.forEach(quiz => {
+            userQuizzesDiv.innerHTML += `
+            <div class="quizz" id="${quiz.id}" onclick="selectUserQuizz(this)">
+                <div class="gradient"></div>
+                <img src="${quiz.image}" alt="">
+                <div class="quizzOption">
+                    <ion-icon name="create-outline"></ion-icon>
+                    <ion-icon onclick="deleteQuizz(this)" name="trash-outline"></ion-icon>
+                </div>
+                <p>${quiz.title}</p>
             </div>
-            <p>${quiz.title}</p>
-        </div>
-        `
-    })
+            `
+        })
+    }
 }
 
+//Deleta quiz
+function deleteQuizz(quiz){
+    //Resgata quizzes de usuário
+    let localQuizzes = JSON.parse(localStorage.getItem('userQuizzes'))
+    //Resgata div quizz que envolve todo o conteúdo de apresentação do quiz
+    let parentDiv = quiz.parentNode.parentNode
+    let parentDivId = parentDiv.id
+    //Percorre cada elemento salvo pra saber qual excluir
+    for(i in localQuizzes){
+        if(localQuizzes[i].id == parentDivId){
+            delete localQuizzes[i]
+            localQuizzes.shift()
+        }
+    }
+    localQuizzes = JSON.stringify(localQuizzes)
+    localStorage.setItem('userQuizzes', localQuizzes)
+}
 //Renderiza todos os quizzes
 function renderQuizzes(quizzes){
     Quizz = quizzes
-    //console.log(Quizz[0].id)
     //Resgata div que contém todos os quizes
     let allQuizzes = document.querySelector('.allQuizzes .quizzes')
     quizzes.forEach(quiz => {
@@ -51,6 +67,7 @@ function renderQuizzes(quizzes){
         </div>
         `
     });
-}   
+}
+
 
 getQuizzes()
